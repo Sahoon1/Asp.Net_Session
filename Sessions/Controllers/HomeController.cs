a43a55e9c82d4c7e6a7cf128a0f19d711b5a2c39
sahoon1 <siblingszahid@gmail.com>
2024-12-18 20:28:30 +0500

    work

---
+++ Sessions/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sessions.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["Var1"] = "Data comes from ViewData";
+            ViewBag.Var2 = "Data comes from ViewBag";
+            TempData["Var3"] = "Data comes from TempData";
+            Session["Var4"] = "Data comes from Session";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            if (Session["Var4"] != null)
+            {
+                Session["Var4"].ToString();
+            }
+
+            return View();
+        }
+
+
+        public ActionResult Contact()
+        {
+            if (Session["Var4"] != null)
+            {
+                Session["Var4"].ToString();
+            }
+
+            return View();
+        }
+    }
+}
